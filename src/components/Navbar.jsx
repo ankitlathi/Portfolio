5e682c0db85a863c8ecbab640a9e62761baca59c
Ankit Lathi <lathiankit1050@gmail.com>
2025-08-26 18:24:01 +0530

    new commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,10 +19,10 @@ export const Navbar =()=>{
         <nav className={`fixed px-20  w-full transition-all duration-100 ${isScrolled ? " bg-slate-900/90 backdrop-blur-md shadow-xl  border-slate-700/50  z-50" : "bg-transparent"}`}>
             <div className="max-w-6xl mx-auto flex justify-between">
                 <div className="text-white font-semibold text-xl py-6">
-                    Ankit Lathi
+                    <a href="#">Ankit Lathi</a>
                 </div>
                 <div className="text-white font-medium py-6 flex gap-8">
-                    <div><a href="#home">Home</a></div>
+                    <div><a href="#">Home</a></div>
                     <div><a href="#skills">Skills</a></div>
                     <div><a href="#projects">Projects</a></div>
                     <div><a href="#contact">Contact</a></div>
